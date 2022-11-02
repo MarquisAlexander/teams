959fefcec433b4d3083ad6704701d9dc9c27e814
marquisalexander <marquis.alexander@outlook.com>
2022-11-02 17:28:10 -0300

    feat():add styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import * as S from "./styles";
 
 export function Groups() {
-  return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<S.Container>
+			<S.Title>Groups</S.Title>
+			<StatusBar style="auto" />
+		</S.Container>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
