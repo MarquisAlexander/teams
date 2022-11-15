e7fcc99cd25d070c2e31f993d8ea306c36789235
marquisalexander <marquis.alexander@outlook.com>
2022-11-15 15:37:02 -0300

    refactor(styles):refactor styled components

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -16,7 +16,7 @@ export const Logo = styled.Image`
 `;
 
 export const BackButton = styled.TouchableOpacity`
-    flex: 1
+    flex: 1;
 `;
 
 export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
