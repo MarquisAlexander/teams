d57cb01e8e0cf9b51c1cd0fc50e01c839d0b6692
marquisalexander <marquis.alexander@outlook.com>
2022-11-15 15:26:48 -0300

    feat(input):add placeholder text color

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -17,7 +17,7 @@ export function NewGroup() {
 					subTitle="Crie uma turma para adicionar as pessoas"
 				/>
 
-				<Input />
+				<Input placeholder="Nome da turma"/>
 
 				<Button title="Criar" style={{ marginTop: 20 }} />
 			</Content>
