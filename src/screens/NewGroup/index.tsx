c53124256058a9181f5cebe3ab8a45f1cd5c3fe8
marquisalexander <marquis.alexander@outlook.com>
2022-11-15 15:20:44 -0300

    feat(component):create input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
 	return (
@@ -16,7 +17,9 @@ export function NewGroup() {
 					subTitle="Crie uma turma para adicionar as pessoas"
 				/>
 
-				<Button title="Criar" />
+				<Input />
+
+				<Button title="Criar" style={{ marginTop: 20 }} />
 			</Content>
 		</Container>
 	);
