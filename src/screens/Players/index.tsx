80663d55c0e393b3cd0967762e8d0ff42b853ccd
marquisalexander <marquis.alexander@outlook.com>
2022-11-16 21:47:58 -0300

    feat(components):create buttonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
@@ -11,6 +12,8 @@ export function Players() {
 				title="Nome da turma"
 				subTitle="adicione a galera e separe os times"
 			/>
+
+			<ButtonIcon />
 		</Container>
 	);
 }
