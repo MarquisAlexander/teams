d4d260751a6862b3c71ff5a85853842e6a502fad
marquisalexander <marquis.alexander@outlook.com>
2022-11-24 20:57:03 -0300

    feat():create playerCard component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,11 +6,13 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { PlayerCard } from "@components/PlayerCard";
+
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
 	const [team, setTeam] = useState("Time A");
-	const [players, setPlayers] = useState([]);
+	const [players, setPlayers] = useState(["Marquis", "Rick"]);
 
 	return (
 		<Container>
@@ -41,6 +43,14 @@ export function Players() {
 				/>
 				<NumberOfPlayers>{players.length}</NumberOfPlayers>
 			</HeaderList>
+
+			<FlatList
+				data={players}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<PlayerCard name={item} onRemove={() => {}} />
+				)}
+			/>
 		</Container>
 	);
 }
