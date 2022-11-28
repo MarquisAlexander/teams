593b72032a9e70d8c4311eb2bc99abf5eb8169be
marquisalexander <marquis.alexander@outlook.com>
2022-11-28 20:23:54 -0300

    feat():added styles to players flatlist

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,10 +9,20 @@ import { Input } from "@components/Input";
 import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
 	const [team, setTeam] = useState("Time A");
-	const [players, setPlayers] = useState(["Marquis", "Rick"]);
+	const [players, setPlayers] = useState([
+		"Marquis",
+		"Rick",
+		"Peppa",
+		"Tonhao",
+		"Julio",
+		"Pedro",
+		"Bernardo",
+	]);
 
 	return (
 		<Container>
@@ -50,7 +60,17 @@ export function Players() {
 				renderItem={({ item }) => (
 					<PlayerCard name={item} onRemove={() => {}} />
 				)}
+				ListEmptyComponent={() => (
+					<ListEmpty message="Não há pessoas nesse time" />
+				)}
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={[
+					{ paddingBottom: 100 },
+					players.length === 0 && { flex: 1 },
+				]}
 			/>
+
+			<Button title="Remover Turma" type="SECONDARY" />
 		</Container>
 	);
 }
