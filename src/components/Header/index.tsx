4ef861e7c6c344e7ac6232ab922bea7c6130d0be
marquisalexander <marquis.alexander@outlook.com>
2022-12-19 20:27:07 -0300

    feat():added navigation between screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import LogoImg from "@assets/logo.png";
 import { Container, Logo, BackButton, BackIcon } from "./styles";
 
@@ -6,10 +8,15 @@ type Props = {
 };
 
 export function Header({ showBackButton = false }: Props) {
+	const navigation = useNavigation();
+	function handleGoBack() {
+		navigation.navigate('groups');
+	}
+
 	return (
 		<Container>
 			{showBackButton && (
-				<BackButton>
+				<BackButton onPress={handleGoBack}>
 					<BackIcon />
 				</BackButton>
 			)}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,6 +8,12 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+	const navigation = useNavigation();
+
+	function handleNew() {
+		navigation.navigate("players", { group: "Panelinha" });
+	}
+
 	return (
 		<Container>
 			<Header showBackButton />
@@ -17,9 +25,9 @@ export function NewGroup() {
 					subTitle="Crie uma turma para adicionar as pessoas"
 				/>
 
-				<Input placeholder="Nome da turma"/>
+				<Input placeholder="Nome da turma" />
 
-				<Button title="Criar" style={{ marginTop: 20 }} />
+				<Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
 			</Content>
 		</Container>
 	);
