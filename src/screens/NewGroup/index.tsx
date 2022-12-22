2e136105ef4c99c87ed0bf7115e634343e3884f4
marquisalexander <marquis.alexander@outlook.com>
2022-12-21 21:26:22 -0300

    feat():create instance error

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
-import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+
+import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
 	const [group, setGroup] = useState("");
@@ -18,7 +20,12 @@ export function NewGroup() {
 			await groupCreate(group);
 			navigation.navigate("players", { group });
 		} catch (error) {
-			console.log(error);
+			if (error instanceof AppError) {
+				Alert.alert("Novo Grupo", error.message);
+			} else {
+				Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+				console.log(error);
+			}
 		}
 	}
 
