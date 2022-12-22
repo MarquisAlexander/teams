5bf8bf72e79061984f5f27efc38a16cc22cbe034
marquisalexander <marquis.alexander@outlook.com>
2022-12-21 21:31:00 -0300

    feat():added navigate to see group

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -29,6 +29,10 @@ export function Groups() {
 		}
 	}
 
+	function handleOpenGroup(group: string) {
+		navigation.navigate("players", { group });
+	}
+
 	useFocusEffect(
 		useCallback(() => {
 			fetchGroups();
@@ -43,7 +47,7 @@ export function Groups() {
 				data={groups}
 				keyExtractor={(item) => item}
 				renderItem={({ item }) => (
-					<GroupCard title={item} onPress={() => console.log("click")} />
+					<GroupCard title={item} onPress={() => handleOpenGroup(item)} />
 				)}
 				contentContainerStyle={groups.length === 0 && { flex: 1 }}
 				ListEmptyComponent={() => (
