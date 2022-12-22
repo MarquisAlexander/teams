55d3adc0b11fb72e45789bc35ab6b2b65bb1d52b
marquisalexander <marquis.alexander@outlook.com>
2022-12-21 21:18:02 -0300

    feat():save at async storage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,26 +1,39 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-
-import * as S from "./styles";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
+import { groupsGetAll } from "@storage/group/groupsGetAll";
+import * as S from "./styles";
+
 export function Groups() {
-	const [groups, setGroups] = useState<string[]>([
-		"Galera da panelinha",
-		"Somente Backenders",
-	]);
+	const [groups, setGroups] = useState<string[]>([]);
 
 	const navigation = useNavigation();
 
 	function handleNewGroup() {
 		navigation.navigate("new");
 	}
+
+	async function fetchGroups() {
+		try {
+			const data = await groupsGetAll();
+			setGroups(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useFocusEffect(
+		useCallback(() => {
+			fetchGroups();
+		}, []),
+	);
 
 	return (
 		<S.Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -7,13 +7,19 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { groupCreate } from "@storage/group/groupCreate";
 
 export function NewGroup() {
 	const [group, setGroup] = useState("");
 	const navigation = useNavigation();
 
-	function handleNew() {
-		navigation.navigate("players", { group });
+	async function handleNew() {
+		try {
+			await groupCreate(group);
+			navigation.navigate("players", { group });
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
 	return (
