aca3e1014a80a0f4a047fde088bb16129f61da24
marquisalexander <marquis.alexander@outlook.com>
2022-12-26 21:28:54 -0300

    feat():create function to remove group by name

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { FlatList, Alert, TextInput } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 
 import { AppError } from "@utils/AppError";
 
@@ -9,6 +9,7 @@ import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
+import { groupRemoveByName } from "@storage/group/groupRemoveByName";
 
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
@@ -30,6 +31,7 @@ export function Players() {
 	const [newPlayerName, setNewPlayerName] = useState("");
 	const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
+	const navigation = useNavigation();
 	const route = useRoute();
 	const { group } = route.params as RouteParams;
 
@@ -90,6 +92,24 @@ export function Players() {
 		}
 	}
 
+	async function groupRemove() {
+		try {
+			await groupRemoveByName(group);
+
+			navigation.navigate("groups");
+		} catch (error) {
+			console.log(error);
+			Alert.alert("Remover grupo", "Não foi possível remover o grupo");
+		}
+	}
+
+	async function handleGroupRemove() {
+		Alert.alert("Remover", "Deseja remover o grupo?", [
+			{ text: "Não", style: "cancel" },
+			{ text: "Sim", onPress: () => groupRemove() },
+		]);
+	}
+
 	useEffect(() => {
 		fetchPlayersByTeam();
 	}, [team]);
@@ -148,7 +168,11 @@ export function Players() {
 				]}
 			/>
 
-			<Button title="Remover Turma" type="SECONDARY" />
+			<Button
+				title="Remover Turma"
+				type="SECONDARY"
+				onPress={handleGroupRemove}
+			/>
 		</Container>
 	);
 }
